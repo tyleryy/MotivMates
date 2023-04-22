dbf65cb106adc201922061e0ca5fb795dbe79d1a
Alicia <aliciachuang2003@gmail.com>
2023-04-22 11:34:46 -0700

    Menu page and other stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Startpage from './Startpage';
+import Menu from './Menu';
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+    // <div className='App'>
+    //   <Startpage></Startpage>
+    // </div>
+
+  // <div>
+  //   <Menu></Menu>
+  // </div>
+  <div>
+    
+  </div>
+  
+ );
 }
 
+
 export default App;
+
+
---
+++ frontend/src/Menu.js
@@ -0,0 +1,120 @@
+
+
+// import growtree from './assets/growtree.gif'
+import './Menu.css';
+
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+function Addfriend() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+        <div className='spacing'>
+            <h1 className="title">MotiveMates</h1>
+        </div>
+        <div className='center'>
+        <Button variant="outlined" onClick={handleClickOpen}>
+            Add a new friend
+        </Button>
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Add Friend</DialogTitle>
+            <DialogContent>
+            <DialogContentText>
+                Search for a friend by their unique username and send add request
+            </DialogContentText>
+            <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Friend Username"
+                type="email"
+                fullWidth
+                variant="standard"
+            />
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleClose}>Add</Button>
+            </DialogActions>
+        </Dialog>
+        </div>
+    </div>
+  );
+}
+
+
+function ToggleButtons() {
+  
+    return (
+      <ToggleButtonGroup
+        exclusive
+        // onChange={change display}
+      >
+        <ToggleButton value="left" aria-label="left aligned">
+          Active Friends
+        </ToggleButton>
+        <ToggleButton value="center" aria-label="centered">
+          Inactive Friends
+        </ToggleButton>
+        <ToggleButton value="right" aria-label="right aligned">
+          Pending Invitations
+        </ToggleButton>
+      </ToggleButtonGroup>
+    );
+  }
+
+
+// make call to database for username
+let username = "placeholder_name"
+
+
+function Menu() {
+ return (
+   <div>
+    <div className='right'>
+        <p>{username}</p>
+    </div>
+    <div>
+        {/* Add friends */}
+        <Addfriend></Addfriend>
+    </div>
+    <div className='center'>
+        {/* Friends */}
+        <ToggleButtons></ToggleButtons>
+    </div>
+    <div>
+        {/* Bottom navigation */}
+        
+    </div>
+   </div>
+    
+ );
+}
+
+
+export default Menu;
+
+
+
+
+
