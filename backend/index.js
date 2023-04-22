da623fadd504e442eedcf1b8b78487b4d6ba94d0
Tyler Yu <tyler.y.yu@gmail.com>
2023-04-22 04:37:57 -0700

    api works

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,44 @@
-const express = require('express')
 
-const app = express();
-const port = 3000;
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "firebase/app";
+ import {getFirestore, collection, getDocs} from 'firebase/firestore/lite';
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+import express from 'express';
+import cors from 'cors'
 
-app.get('/', (req, res) => {
+ 
+ const API = express();
+ API.use(cors());
+ const port = 3000;
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA3TwYE8yiEm7e7s80kHPFfnXLDvuBF1Sg",
+    authDomain: "motivmates.firebaseapp.com",
+    databaseURL: "https://motivmates-default-rtdb.firebaseio.com",
+    projectId: "motivmates",
+    storageBucket: "motivmates.appspot.com",
+    messagingSenderId: "693369238340",
+    appId: "1:693369238340:web:7a3cc222a0754e6b805d11",
+    measurementId: "G-XRLEV03ZNK"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app)
+
+API.get('/', (req, res) => {
     res.send('health check');
 });
-app.get('/firestore', (req,res) => {
-    
+
+API.get('/firestore', async (req,res) => {
+    const user_data = collection(db, 'relations');
+    const query = await getDocs(user_data);
+    const query_list = query.docs.map(doc=>doc.data());
+    console.log(query_list);
+    return query_list; 
 })
 
-app.listen(port, () => {
+API.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })
--- project/App.js
+++ project/App.js
@@ -1,43 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "firebase/app";
- import { getAnalytics } from "firebase/analytics";
- import {getFirestore, collection, getDocs} from 'firebase/firestore/lite'
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
 import { useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
- 
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyA3TwYE8yiEm7e7s80kHPFfnXLDvuBF1Sg",
-    authDomain: "motivmates.firebaseapp.com",
-    databaseURL: "https://motivmates-default-rtdb.firebaseio.com",
-    projectId: "motivmates",
-    storageBucket: "motivmates.appspot.com",
-    messagingSenderId: "693369238340",
-    appId: "1:693369238340:web:7a3cc222a0754e6b805d11",
-    measurementId: "G-XRLEV03ZNK"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const db = getFirestore(app)
-
-  const queryFunc = async () => {
-    const user_data = collection(db, 'relations');
-    const query = await getDocs(user_data);
-    const query_list = query.docs.map(doc=>doc.data());
-    console.log(query_list);
+  const test = async () => {
+    const res = await axios.get('http://localhost:3000/');
+    console.log(res.data)
   }
 
   useEffect( () => {
-    queryFunc();
-  }, [] )
+    test();
+  }, [])
 
   
   return (
