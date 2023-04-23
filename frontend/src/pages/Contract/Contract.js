2c088f8f37f0ead0af4501d4c1b5a593016f7522
Tyler Yu <tyler.y.yu@gmail.com>
2023-04-23 09:40:41 -0700

    contact_id

--- frontend/src/pages/Contract/Contract.js
+++ frontend/src/pages/Contract/Contract.js
@@ -116,12 +116,11 @@ function Contract() {
     const app = global_vars.app;
     const RTdatabase = getDatabase(app)
 
-    // useEffect(() => {
+    useEffect(() => {
 
 
       // const contract_listener = ref(RTdatabase, 'users/' + )
-    //   const contract_listener = ref(RTdatabase, 'users/' + )
-    // }, [])
+    }, [])
 
     // set(ref(RTdatabase, 'users/', localStorage.getItem("email")), {
     //   username: name
--- frontend/src/pages/Menu/Friendlist/Friendlist.js
+++ frontend/src/pages/Menu/Friendlist/Friendlist.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { useNavigate } from "react-router-dom";
 import { Button, List, ListItem, Grid, Card, CardActionArea } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { Context } from '../../../providers/provider';
+import {setDoc, doc, updateDoc} from 'firebase/firestore';
+
 
 function PendingCell({ value }) {
     const PendingCard = styled(Card)(({ theme }) => ({
@@ -10,12 +13,44 @@ function PendingCell({ value }) {
         margin: 10
         }));
 
+    const globals = React.useContext(Context);
+    const db = globals.db;
+    const config = globals.config;
+    const email = localStorage.getItem("email")
+    
+    const generateLobbyCode = () => {
+        let result;
+    
+            result = '';
+            let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+            for ( let i = 0; i < 5; i++ ) {
+                result += characters.charAt(Math.floor(Math.random() * characters.length));
+            }
+    
+        return result;
+    }
+
+    const fetchData = async () => {
+        let x = generateLobbyCode()
+        console.log(x)
+        await updateDoc(doc(db, 'relations', email), {
+            "contract_id": x
+          })
+          await updateDoc(doc(db, 'relations', value), {
+            "contract_id": x
+          })
+    }
+
+    const handleClick = () => {
+        fetchData()
+    }
+
     return (
         <PendingCard raised="true">
             <ListItem sx={{ height: "100%", width: "100%", alignItems: "center" }}>
                 <Grid container spacing={2}>
                     <Grid item xs={8}> {value} </Grid>
-                    <Grid item xs={2}> <Button variant="contained" color="secondary">Accept</Button> </Grid>
+                    <Grid item xs={2}> <Button onClick={handleClick} variant="contained" color="secondary">Accept</Button> </Grid>
                     <Grid item xs={2}> <Button variant="contained">Reject</Button> </Grid>
                 </Grid>
             </ListItem>
